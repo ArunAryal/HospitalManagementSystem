e20d02f6b25947b24b8c6ad474722248a48ca6b3
ArunAryal <aryalashim55@gmail.com>
2026-03-25 06:50:58 +0545

    populate __init__.py

---
+++ backend/routers/__init__.py
@@ -0,0 +1,17 @@
+from . import patients
+from . import doctors
+from . import appointments
+from . import medical_records
+from . import billing
+from . import medicines
+from . import rooms
+
+__all__ = [
+    "patients",
+    "doctors",
+    "appointments",
+    "medical_records",
+    "billing",
+    "medicines",
+    "rooms",
+]
